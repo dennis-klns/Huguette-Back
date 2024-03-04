81e756652dacd2b001f00f770edc836d9d105f8b
Dessline <desailles.dessline@outlook.fr>
2024-03-04 17:28:22 +0100

    routes trips et users

--- routes/trips.js
+++ routes/trips.js
@@ -13,12 +13,12 @@ router.post('/', (req, res) => {
         const newTrip = new Trip({
             passenger: req.body.passenger,
             departure: {
-                longitude:req.body.longitudeD,
-                latitude:req.body.latitudeD,
+                longitudeD:req.body.longitudeD,
+                latitudeD:req.body.latitudeD,
             },
             arrival: {
-                longitude:req.body.longitudeA,
-                latitude:req.body.latitudeA,
+                longitudeA:req.body.longitudeA,
+                latitudeA:req.body.latitudeA,
             },
             date: new Date,
         });
@@ -27,6 +27,49 @@ router.post('/', (req, res) => {
           res.json({ result: true, trip: newDoc });
         }).catch(error => res.json({ result: false, error: 'Database error', details: error }));
       
+  });
+
+  router.put('/costposition', (req, res) => {
+    if (!checkBody(req.body, ['cost', 'tripId'])) {
+      return res.json({ result: false, error: 'Missing or empty fields' });
+    }
+  
+      Trip.updateOne(
+        { _id: req.body.tripId },
+        { cost: req.body.cost, departure: { longitudeD: req.body.longitudeD, latitudeD : req.body.latitudeD}}
+       ).then(() => {
+        
+          Trip.findOne({ _id: req.body.tripId }).then(data => {
+            return res.json({ cost: data.cost, departure: data.departure});
+        }).catch(error => res.json({ result: false, error: 'Database error', details: error }));
+       
+       });
+      
+  });
+
+
+  router.put('/drivervalidation', (req, res) => {
+    if (!checkBody(req.body, ['driverId', 'tripId'])) {
+      return res.json({ result: false, error: 'Missing or empty fields' });
+    }
+  
+      Trip.updateOne(
+        { _id: req.body.tripId },
+        { driver: req.body.driverId}
+       ).then(() => {
+        
+          Trip.findOne({ _id: req.body.tripId }).populate('driver').then(data => {
+            return res.json({ trip: data.driver});
+        }).catch(error => res.json({ result: false, error: 'Database error', details: error }));
+       
+       });
+      
+  });
+
+  router.get('/:tripId', function(req, res) {
+    Trip.findById(req.params.tripId).populate('driver passenger').then(data => {
+        return res.json({ trip: data});
+       });
   });
 
 module.exports = router;
