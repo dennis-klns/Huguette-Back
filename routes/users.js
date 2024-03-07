8fbecfac4fb07afdd5edfa47f7454fb0fcac1902
Dessline <desailles.dessline@outlook.fr>
2024-03-07 10:26:06 +0100

    added firstname to what we get from route signin

--- routes/users.js
+++ routes/users.js
@@ -54,7 +54,7 @@ router.post('/signIn', (req, res) => {
 
   User.findOne(searchCriteria).then(data => {
     if (data && bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, token: data.token });
+      res.json({ result: true, token: data.token, firstname: data.firstname });
     } else {
       res.json({ result: false, error: 'User not found or wrong password' });
     }
