9fccd4e2a469b5b8ef580401041b19da0f94c28e
Your name <Your email>
2024-03-10 10:57:52 +0100

    création de la route driver sign up

--- routes/users.js
+++ routes/users.js
@@ -41,6 +41,40 @@ router.post('/signUp', (req, res) => {
   });
 });
 
+router.post('/signUpDriver', (req, res) => {
+  if (!checkBody(req.body, ['lastname','firstname', 'phone', 'email', 'password', 'birthdate', 'gender'])) {
+    res.json({ result: false, error: 'Missing or empty fields' });
+    return;
+  }
+
+  // rajout de la route pour créer un driver
+  User.findOne({ phone: req.body.phone }).then(data => {
+    if (data === null) {
+      const hash = bcrypt.hashSync(req.body.password, 10);
+
+      const newUser = new User({
+        lastname: req.body.lastname,
+        firstname: req.body.firstname,
+        email: req.body.email,
+        phone: req.body.phone,
+        birthdate: req.body.birthdate,
+        gender: req.body.gender,
+        password: hash,
+        token: uid2(32),
+        type: 'driver',
+        isBanned: false,
+      });
+
+      newUser.save().then(newDoc => {
+        res.json({ result: true, token: newDoc.token });
+      });
+    } else {
+      // User already exists in database
+      res.json({ result: false, error: 'User already exists' });
+    }
+  });
+});
+
 
 
 router.post('/signIn', (req, res) => {
