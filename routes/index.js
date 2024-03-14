8c411b3d24961f2395afefdce590358d171ddd55
Your name <Your email>
2024-03-14 14:05:37 +0100

    ajout route delete photo

--- routes/index.js
+++ routes/index.js
@@ -64,6 +64,27 @@ router.post('/uploadLibrairie', async (req, res) => {
 });
 
 
+router.delete('/deletePhoto/:token', async (req, res) => {
+  try {
+      // Logique pour trouver l'utilisateur par token et supprimer la photo
+      const user = await User.findOne({ token: req.params.token });
+      if (user && user.picture) {
+          // Supprimez la photo de Cloudinary en utilisant l'ID de l'image ou un autre identifiant unique si nécessaire
+          // cloudinary.uploader.destroy(imageId);
+
+          // Supprimez ou mettez à jour l'URL de l'image dans la base de données de l'utilisateur
+          await User.updateOne({ token: req.params.token }, { $set: { picture: null } });
+
+          res.json({ result: true, message: "Photo supprimée avec succès." });
+      } else {
+          res.status(404).json({ result: false, message: "Utilisateur non trouvé ou aucune photo à supprimer." });
+      }
+  } catch (error) {
+      console.error(error);
+      res.status(500).json({ result: false, message: "Erreur lors de la suppression de la photo." });
+  }
+});
+
 
 // router.post('/upload', async (req, res) => {
 //   try {
